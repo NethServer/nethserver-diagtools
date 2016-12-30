54efa4ccdb74f77a3e9e4639a5558314f361c5cd
Stephane de Labrusse <stephdl@de-labrusse.fr>
2016-12-30 09:52:13 +0100

    external ip

---
+++ root/usr/share/nethesis/NethServer/Template/DiagTools/ExternalIP.php
@@ -0,0 +1,20 @@
+<?php
+/* @var $view Nethgui\Renderer\Xhtml */
+echo $view->header()->setAttribute('template', $T('DiagToolsIpAdd_header'));
+
+//echo $view->buttonList()
+//    ->insert($view->button('Run', $view::BUTTON_SUBMIT));
+//    ->insert($view->button('Help', $view::BUTTON_HELP));
+
+echo "<pre class='DiagTools_ExternalIP'>";
+echo $view->textLabel('report');
+echo "</pre>";
+
+
+$view->includeCss('
+    pre.DiagTools_ExternalIP {
+        border: 2px solid #aaa;
+        padding: 10px;
+        width: 750px;
+    }
+');
